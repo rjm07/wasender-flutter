6d4387d1e633b951f675dd694c6ca89917a8b0e2
Jake Manuel <rjm.ideas@gmail.com>
2025-01-21 11:38:29 +0800

    Insert formTextfield to Form

--- lib/app/ui/pages/feature_login/change_password_screen.dart
+++ lib/app/ui/pages/feature_login/change_password_screen.dart
@@ -73,107 +73,110 @@ class _State extends State<ChangePasswordScreen> {
           },
         ),
       ),
-      body: Container(
-        padding: const EdgeInsets.fromLTRB(20, 64, 20, 20),
-        width: double.infinity,
-        height: double.infinity,
-        color: Colors.white,
-        child: Center(
-          child: Column(
-            spacing: 20,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Column(
-                spacing: 20,
-                children: [
-                  Column(
-                    spacing: 8,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Keamanan Akun',
-                        style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.w400, color: Colors.black87),
-                      ),
-                      Text(
-                        'Untuk menjara keamanan akun, silahkan masukkan kata sandi baru.',
-                        style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400, color: Colors.black87),
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Column(
-                        spacing: 20,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          LoginTextField(
-                              label: "Kata Sandi",
-                              hintText: "Kata Sandi",
-                              isObscure: true,
-                              controller: passwordController,
-                              validator: (String? value) {
-                                if (value == null || value.isEmpty) {
-                                  setState(() {
-                                    _isAuthenticating = false;
-                                  });
-                                  return "This field is required";
-                                } else {
-                                  return null;
-                                }
-                              },
-                              onChanged: (text) {}),
-                          LoginTextField(
-                              label: "Konfirmasi Kata Sandi",
-                              hintText: "Konfirmasi Kata Sandi",
-                              isObscure: true,
-                              controller: confirmPasswordController,
-                              validator: (String? value) {
-                                if (value == null || value.isEmpty) {
-                                  setState(() {
-                                    _isAuthenticating = false;
-                                  });
-                                  return "This field is required";
-                                } else if (value != passwordController.text) {
-                                  setState(() {
-                                    _isAuthenticating = false;
-                                  });
-                                  return "Password and confirm password must be the same";
-                                } else {
-                                  return null;
-                                }
-                              },
-                              onChanged: (text) {}),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              WidgetButton(
-                onPressed: onSimpanDanLanjutPressed,
-                child: _isAuthenticating
-                    ? const Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SizedBox(height: 18.0, width: 18.0, child: CircularProgressIndicator(color: Colors.white)),
-                          SizedBox(
-                            width: 24.0,
-                          ),
-                          Text(
-                            "Please wait...",
-                            style: TextStyle(fontSize: 12.0, color: Colors.white),
-                          ),
-                        ],
-                      )
-                    : const Text(
-                        "Simpan dan Lanjut",
-                        style: TextStyle(fontSize: 14.0, color: Colors.white),
-                      ),
-              ),
-            ],
+      body: Form(
+        key: formKey,
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(20, 64, 20, 20),
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.white,
+          child: Center(
+            child: Column(
+              spacing: 20,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  spacing: 20,
+                  children: [
+                    Column(
+                      spacing: 8,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Keamanan Akun',
+                          style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.w400, color: Colors.black87),
+                        ),
+                        Text(
+                          'Untuk menjara keamanan akun, silahkan masukkan kata sandi baru.',
+                          style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400, color: Colors.black87),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Column(
+                          spacing: 20,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            LoginTextField(
+                                label: "Kata Sandi",
+                                hintText: "Kata Sandi",
+                                isObscure: true,
+                                controller: passwordController,
+                                validator: (String? value) {
+                                  if (value == null || value.isEmpty) {
+                                    setState(() {
+                                      _isAuthenticating = false;
+                                    });
+                                    return "This field is required";
+                                  } else {
+                                    return null;
+                                  }
+                                },
+                                onChanged: (text) {}),
+                            LoginTextField(
+                                label: "Konfirmasi Kata Sandi",
+                                hintText: "Konfirmasi Kata Sandi",
+                                isObscure: true,
+                                controller: confirmPasswordController,
+                                validator: (String? value) {
+                                  if (value == null || value.isEmpty) {
+                                    setState(() {
+                                      _isAuthenticating = false;
+                                    });
+                                    return "This field is required";
+                                  } else if (value != passwordController.text) {
+                                    setState(() {
+                                      _isAuthenticating = false;
+                                    });
+                                    return "Password and confirm password must be the same";
+                                  } else {
+                                    return null;
+                                  }
+                                },
+                                onChanged: (text) {}),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                WidgetButton(
+                  onPressed: onSimpanDanLanjutPressed,
+                  child: _isAuthenticating
+                      ? const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(height: 18.0, width: 18.0, child: CircularProgressIndicator(color: Colors.white)),
+                            SizedBox(
+                              width: 24.0,
+                            ),
+                            Text(
+                              "Please wait...",
+                              style: TextStyle(fontSize: 12.0, color: Colors.white),
+                            ),
+                          ],
+                        )
+                      : const Text(
+                          "Simpan dan Lanjut",
+                          style: TextStyle(fontSize: 14.0, color: Colors.white),
+                        ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
